61f9df5ffd5ba3ee5f7189ef005b894443b961ea
Fluffymuzzy <77502156+Fluffymuzzy@users.noreply.github.com>
2023-03-11 20:59:32 +0200

    some changes in MovieList componrnt

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,9 +1,11 @@
 import { FC } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { Movie } from "@/store/movieSlice";
+import Link from "next/link";
+import Image from "next/image";
+import { FaHeart } from "react-icons/fa";
+import { Movie, selectMovie } from "@/store/movieSlice";
 import { RootState } from "@/store/store";
 import { isValidImageUrl } from "@/utils/imageUrlValidation";
-import Image from "next/image";
 
 export interface MovieListProps {
   searchResults: Movie[];
@@ -11,6 +13,11 @@ export interface MovieListProps {
 
 const MovieList: FC<MovieListProps> = ({ searchResults }) => {
   const { status } = useSelector((state: RootState) => state.movie);
+  const dispatch = useDispatch();
+
+  const handleMovieClick = (movie: Movie) => {
+    dispatch(selectMovie(movie));
+  };
 
   if (status === "loading") {
     return <p>Loading search results...</p>;
@@ -24,24 +31,39 @@ const MovieList: FC<MovieListProps> = ({ searchResults }) => {
     <div className="pt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
       {searchResults.map((movie) => (
         <div key={movie.imdbID} className="bg-white rounded-lg shadow-lg">
-          <div className="w-full h-48 overflow-hidden">
-            {isValidImageUrl(movie.poster) ? (
-              <Image
-                width={300}
-                height={450}
-                className="w-full h-full object-contain"
-                src={movie.poster}
-                alt={movie.title}
-              />
-            ) : (
-              <div className="w-full h-full flex justify-center items-center">
-                <p className="text-gray-400">Image not found</p>
-              </div>
-            )}
-          </div>
-          <div className="p-4">
-            <h2 className="text-lg font-medium mb-2">{movie.title}</h2>
-            <p className="text-gray-400">{movie.year}</p>
+          <Link href={`/movie/${movie.imdbID}`}>
+            <div
+              onClick={() => handleMovieClick(movie)}
+              className="w-full h-48 overflow-hidden"
+            >
+              {isValidImageUrl(movie.poster) ? (
+                <Image
+                  width="0"
+                  height="0"
+                  sizes="100vw"
+                  className="w-full h-full object-contain"
+                  src={movie.poster}
+                  alt={movie.title}
+                />
+              ) : (
+                <div className="w-full h-full flex justify-center items-center">
+                  <p className="text-gray-400">Image not found</p>
+                </div>
+              )}
+            </div>
+          </Link>
+          <div className="flex justify-between items-center">
+            <div className="p-4">
+              <Link href={`/movie/${movie.imdbID}`}>
+                <h2 className="text-lg font-medium mb-2">{movie.title}</h2>
+              </Link>
+              <p className="text-gray-400">{movie.year}</p>
+            </div>
+            <div className="p-4">
+              <button className="w-4 h-4">
+                <FaHeart className="text-black-500 mr-2" />
+              </button>
+            </div>
           </div>
         </div>
       ))}
