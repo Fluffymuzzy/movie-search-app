5b598c3c7e1c4a39128ea7a2920032b9089ac919
Fluffymuzzy <77502156+Fluffymuzzy@users.noreply.github.com>
2023-03-11 17:48:32 +0200

    add functions to handle user input and search for movies using Redux

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,11 +1,30 @@
-import React from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 import { FaSearch } from "react-icons/fa";
+import { searchMovies } from "@/store/movieSlice";
+import { AppDispatch } from "@/store/store";
 
 const SearchBar = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const [query, setQuery] = useState("");
+
+  const handleSearch = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (query.trim()) {
+      dispatch(searchMovies(query));
+    }
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
+
   return (
-    <form className="relative">
+    <form className="relative" onSubmit={handleSearch}>
       <input
         type="text"
+        value={query}
+        onChange={handleInputChange}
         placeholder="Search for a movie"
         className="py-2 pr-8 pl-4 rounded-full border-2 border-gray-200 focus:outline-none focus:border-blue-500"
       />
