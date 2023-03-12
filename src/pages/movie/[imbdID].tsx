aaf77aee4156c75e335c59253e8cf65763dd9c7e
Fluffymuzzy <77502156+Fluffymuzzy@users.noreply.github.com>
2023-03-12 17:22:22 +0200

    add func addFavorite to MovieDetails

--- src/pages/movie/[imbdID].tsx
+++ src/pages/movie/[imbdID].tsx
@@ -1,16 +1,31 @@
-import { useSelector } from "react-redux";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Image from "next/image";
 import Link from "next/link";
 import { TiArrowBack } from "react-icons/ti";
+import { FaHeart, FaCheck } from "react-icons/fa";
 import { RootState } from "@/store/store";
+import { addFavorite } from "@/store/favoriteSlice";
+import { Movie, selectMovie } from "@/store/movieSlice";
 import Header from "@/components/Header";
 
 const MovieDetails = () => {
   const movie = useSelector((state: RootState) => state.movie.selectedMovie);
+  const [addedMovieId, setAddedMovieId] = useState<string>("");
+  const favorites = useSelector((state: RootState) => state.favorite.movies);
+  const dispatch = useDispatch();
 
   if (!movie) {
     return <div>Loading movie details...</div>;
   }
+
+  const handleAddFavorite = (movie: Movie) => {
+    dispatch(addFavorite(movie));
+    setAddedMovieId(movie.imdbID);
+  };
+
+  const isFavorite = (movie: Movie) =>
+    favorites.some((favorite) => favorite.imdbID === movie.imdbID);
 
   return (
     <>
@@ -48,9 +63,25 @@ const MovieDetails = () => {
           <h2 className="text-lg font-bold mb-2">Director</h2>
           <p className="text-gray-500">{movie.director}</p>
         </div>
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center sm:mr-8">
           <h2 className="text-lg font-bold mb-2">Ratings</h2>
           <p className="text-gray-500">{movie.ratings}</p>
+        </div>
+        <div className="flex flex-col items-center ml-8">
+          {isFavorite(movie) ? (
+            <FaCheck className="text-red-500 mr-2" />
+          ) : (
+            <button
+              className="w-4 h-4"
+              onClick={() => handleAddFavorite(movie)}
+            >
+              {addedMovieId === movie.imdbID ? (
+                <FaCheck className="text-red-500 mr-2" />
+              ) : (
+                <FaHeart className="text-black-500 mr-2" />
+              )}
+            </button>
+          )}
         </div>
       </div>
     </>
