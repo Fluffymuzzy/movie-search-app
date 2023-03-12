1a53f22dee03301e710c4e3b2f8ec82d1b5cbf91
Fluffymuzzy <77502156+Fluffymuzzy@users.noreply.github.com>
2023-03-12 14:55:27 +0200

    add fonts and favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,19 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { store } from "@/store/store";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "700", "800"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />;
+      <main className={poppins.className}>
+        <Component {...pageProps} />;
+      </main>
     </Provider>
   );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,23 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Movie Search App</title>
+        <meta name="description" content="Search for your favorite movies" />
+        <meta name="keywords" content="movies, search, favorites" />
+        <meta name="author" content="Fluffymuzzy" />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://fonts.googleapis.com/css?family=Roboto:400,500&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
