9301bc6335571cd3729437d708dfa36fcc22fdd5
Fluffymuzzy <77502156+Fluffymuzzy@users.noreply.github.com>
2023-03-12 17:34:07 +0200

    some changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { store } from "@/store/store";
 import { Poppins } from "next/font/google";
-
+import Head from "next/head";
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "700", "800"],
@@ -13,6 +13,13 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <main className={poppins.className}>
+      <Head>
+        <title>Movie Search App</title>
+        <meta name="description" content="Search for your favorite movies" />
+        <meta name="keywords" content="movies, search, favorites" />
+        <meta name="author" content="Fluffymuzzy" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
         <Component {...pageProps} />;
       </main>
     </Provider>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,13 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>Movie Search App</title>
-        <meta name="description" content="Search for your favorite movies" />
-        <meta name="keywords" content="movies, search, favorites" />
-        <meta name="author" content="Fluffymuzzy" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
